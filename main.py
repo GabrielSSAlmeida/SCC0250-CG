5c043546c8ef86ca670e671b40a12f91b8d834de
Camila Donda <camila.donda@usp.br>
2025-05-08 21:21:17 -0300

    feat: ambiente interno decorado uhuu

--- main.py
+++ main.py
@@ -44,6 +44,11 @@ def main():
     vi_chao, n_chao, tex_chao = FileManager.load_obj_and_texture('objects/chao.obj', ['textures/Jardin_raices_Diffuse.png'])
     vi_pomo, n_pomo, tex_pomo = FileManager.load_obj_and_texture('objects/pomo.obj', ['textures/pomo.jpeg'])
     vi_globo, n_globo, tex_globo = FileManager.load_obj_and_texture('objects/globo.obj', ['textures/globo.png'])
+    vi_mesa, n_mesa, tex_mesa = FileManager.load_obj_and_texture('objects/mesa.obj', ['textures/Madera_puerta_Albedo.png'])
+    vi_sapo, n_sapo, tex_sapo = FileManager.load_obj_and_texture('objects/sapo.obj', ['textures/sapo.png'])
+    vi_cartas, n_cartas, tex_cartas = FileManager.load_obj_and_texture('objects/cartas.obj', ['textures/cartas.png'])
+    vi_caixa, n_caixa, tex_caixa = FileManager.load_obj_and_texture('objects/caixa.obj', ['textures/caixa.png'])
+    vi_cadeira, n_cadeira, tex_cadeira = FileManager.load_obj_and_texture('objects/cadeira.obj', ['textures/Madera_puerta_Albedo.png'])
 
 
     base_casa = Model_3D(vi_base, n_base, DEFAULT_HUT, tex_base)
@@ -53,6 +58,11 @@ def main():
     chao = Model_3D(vi_chao, n_chao, CHAO, tex_chao)
     pomo = Model_3D(vi_pomo, n_pomo, POMO, tex_pomo)
     globo = Model_3D(vi_globo, n_globo, GLOBO, tex_globo)
+    mesa = Model_3D(vi_mesa, n_mesa, MESA, tex_mesa)
+    sapo = Model_3D(vi_sapo, n_sapo, SAPO, tex_sapo)
+    caixa = Model_3D(vi_caixa, n_caixa, CAIXA, tex_caixa)
+    cartas = Model_3D(vi_cartas, n_cartas, CARTAS, tex_cartas)
+    cadeira = Model_3D(vi_cadeira, n_cadeira, CADEIRA, tex_cadeira)
    
 
     # ====== KEY MANAGER ======
@@ -60,7 +70,8 @@ def main():
     mousemanager = MouseManager(window, view, projection)
 
     # prepare models to render
-    renderer.add_model([base_casa, teto, portas, escada, chao, globo, pomo])
+    renderer.add_model([base_casa, teto, portas, escada, 
+        cadeira, chao, globo, pomo, mesa, sapo, cartas, caixa])
 
     
     # show windows
