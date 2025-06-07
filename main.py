3d36ff5b748f159ab226a1d6f8413ace460d52a1
Gabriel Almeida <gabrielsousa230@gmail.com>
2025-06-07 16:09:29 -0300

    add new models

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 """ 
     SCC0250 - Computação Gráfica
 
-    Projeto 2
+    Projeto 3
 
     Camila Donda Ronchi - 13672220
     Gabriel Sousa Santos de Almeida - 13837432
@@ -41,9 +41,9 @@ def main():
     vi_portas, n_portas, tex_portas = FileManager.load_obj_and_texture('objects/portas.obj', ['textures/Madera_puerta_Albedo.png'])
     vi_escada, n_escada, tex_escada = FileManager.load_obj_and_texture('objects/escada.obj', ['textures/Madera_clara_Albedo.png'])
     vi_chao, n_chao, tex_chao = FileManager.load_obj_and_texture('objects/chao.obj', ['textures/Jardin_raices_Diffuse.png'])
-    vi_pomo, n_pomo, tex_pomo = FileManager.load_obj_and_texture('objects/pomo.obj', ['textures/pomo.jpeg'])
+    # vi_pomo, n_pomo, tex_pomo = FileManager.load_obj_and_texture('objects/pomo.obj', ['textures/pomo.jpeg'])
     vi_globo, n_globo, tex_globo = FileManager.load_obj_and_texture('objects/globo.obj', ['textures/globo.png'])
-    vi_nimbus, n_nimbus, tex_nimbus = FileManager.load_obj_and_texture('objects/nimbus.obj', ['textures/nimbus.jpeg'])
+    # vi_nimbus, n_nimbus, tex_nimbus = FileManager.load_obj_and_texture('objects/nimbus.obj', ['textures/nimbus.jpeg'])
     vi_tree, n_tree, tex_tree = FileManager.load_obj_and_texture('objects/tree.obj', ['textures/tree.png'])
     vi_abobora, n_abobora, tex_abobora = FileManager.load_obj_and_texture('objects/abobora.obj', ['textures/abobora.png'])
     vi_mesa, n_mesa, tex_mesa = FileManager.load_obj_and_texture('objects/mesa.obj', ['textures/Madera_puerta_Albedo.png'])
@@ -51,8 +51,11 @@ def main():
     vi_cartas, n_cartas, tex_cartas = FileManager.load_obj_and_texture('objects/cartas.obj', ['textures/cartas.png'])
     vi_caixa, n_caixa, tex_caixa = FileManager.load_obj_and_texture('objects/caixa.obj', ['textures/caixa.png'])
     vi_cadeira, n_cadeira, tex_cadeira = FileManager.load_obj_and_texture('objects/cadeira.obj', ['textures/Madera_puerta_Albedo.png'])
-    vi_poste, n_poste, tex_poste = FileManager.load_obj_and_texture('objects/lamp_street_.obj', ['textures/lamp_street.png'])
     vi_cubo, n_cubo, tex_cubo = FileManager.load_obj_and_texture('objects/cubo_teste.obj', ['textures/caixa.png'])
+    vi_poste, n_poste, tex_poste = FileManager.load_obj_and_texture('objects/lamp_street_.obj', ['textures/lamp_street.png'])
+    vi_trofeu, n_trofeu, tex_trofeu = FileManager.load_obj_and_texture('objects/trofeu.obj', ['textures/trofeu.png'])
+    vi_varinha, n_varinha, tex_varinha = FileManager.load_obj_and_texture('objects/varinha.obj', ['textures/varinha.png'])
+    vi_lamparina, n_lamparina, tex_lamparina = FileManager.load_obj_and_texture('objects/lamparina.obj', ['textures/lamparina.png'])
     
 
     base_casa = Model_3D(vi_base, n_base, DEFAULT_HUT, tex_base)
@@ -60,15 +63,18 @@ def main():
     portas = Model_3D(vi_portas, n_portas, DEFAULT_HUT, tex_portas)
     escada = Model_3D(vi_escada, n_escada, DEFAULT_HUT, tex_escada)
     chao = Model_3D(vi_chao, n_chao, CHAO, tex_chao)
-    pomo = Model_3D(vi_pomo, n_pomo, POMO, tex_pomo)
+    # pomo = Model_3D(vi_pomo, n_pomo, POMO, tex_pomo)
     globo = Model_3D(vi_globo, n_globo, GLOBO, tex_globo)
-    nimbus = Model_3D(vi_nimbus, n_nimbus, NIMBUS, tex_nimbus)
+    # nimbus = Model_3D(vi_nimbus, n_nimbus, NIMBUS, tex_nimbus)
     mesa = Model_3D(vi_mesa, n_mesa, MESA, tex_mesa)
     sapo = Model_3D(vi_sapo, n_sapo, SAPO, tex_sapo)
     caixa = Model_3D(vi_caixa, n_caixa, CAIXA, tex_caixa)
     cartas = Model_3D(vi_cartas, n_cartas, CARTAS, tex_cartas)
     cadeira = Model_3D(vi_cadeira, n_cadeira, CADEIRA, tex_cadeira)
     poste = Model_3D(vi_poste, n_poste, POSTE, tex_poste)
+    trofeu = Model_3D(vi_trofeu, n_trofeu, TROFEU, tex_trofeu)
+    varinha = Model_3D(vi_varinha, n_varinha, VARINHA, tex_varinha)
+    lamparina = Model_3D(vi_lamparina, n_lamparina, LAMPARINA, tex_lamparina)
     
     
 
@@ -100,16 +106,19 @@ def main():
         escada, 
         chao, 
         globo, 
-        pomo, 
-        nimbus, 
+        # pomo, 
+        # nimbus, 
         mesa, 
         sapo, 
         cartas, 
         caixa,
         cadeira,
-        poste,
         cuboIn,
         cuboEx,
+        poste,
+        trofeu,
+        varinha,
+        lamparina,
     ] + trees + aboboras)
 
 
@@ -118,17 +127,17 @@ def main():
     keymanager.set_key(glfw.KEY_DOWN, make_scaler("down", 1.1, 0.15), aboboras[0])
 
     # POMO DE OURO
-    keymanager.set_key(glfw.KEY_O, lambda m: m.rotate(5, 1, 0, 0), pomo)
-    keymanager.set_key(glfw.KEY_I, lambda m: m.rotate(5, 0, 1, 0), pomo)
-    keymanager.set_key(glfw.KEY_U, lambda m: m.rotate(5, 0, 0, 1), pomo)
+    # keymanager.set_key(glfw.KEY_O, lambda m: m.rotate(5, 1, 0, 0), pomo)
+    # keymanager.set_key(glfw.KEY_I, lambda m: m.rotate(5, 0, 1, 0), pomo)
+    # keymanager.set_key(glfw.KEY_U, lambda m: m.rotate(5, 0, 0, 1), pomo)
 
-    # NIMBUS
-    keymanager.set_key(glfw.KEY_KP_4, lambda m: nimbus_translation(m, dx=-0.3, angle_y=180), nimbus)   
-    keymanager.set_key(glfw.KEY_KP_6, lambda m: nimbus_translation(m, dx=0.3, angle_x=90), nimbus)   
-    keymanager.set_key(glfw.KEY_KP_2, lambda m: nimbus_translation(m, dy=-0.3, angle_z=270), nimbus)  
-    keymanager.set_key(glfw.KEY_KP_5, lambda m: nimbus_translation(m, dy=0.3, angle_z=90), nimbus)   
-    keymanager.set_key(glfw.KEY_KP_1, lambda m: nimbus_translation(m, dz=-0.3, angle_y=90), nimbus)    
-    keymanager.set_key(glfw.KEY_KP_3, lambda m: nimbus_translation(m, dz=0.3, angle_y=270), nimbus)  
+    # # NIMBUS
+    # keymanager.set_key(glfw.KEY_KP_4, lambda m: nimbus_translation(m, dx=-0.3, angle_y=180), nimbus)   
+    # keymanager.set_key(glfw.KEY_KP_6, lambda m: nimbus_translation(m, dx=0.3, angle_x=90), nimbus)   
+    # keymanager.set_key(glfw.KEY_KP_2, lambda m: nimbus_translation(m, dy=-0.3, angle_z=270), nimbus)  
+    # keymanager.set_key(glfw.KEY_KP_5, lambda m: nimbus_translation(m, dy=0.3, angle_z=90), nimbus)   
+    # keymanager.set_key(glfw.KEY_KP_1, lambda m: nimbus_translation(m, dz=-0.3, angle_y=90), nimbus)    
+    # keymanager.set_key(glfw.KEY_KP_3, lambda m: nimbus_translation(m, dz=0.3, angle_y=270), nimbus)  
 
 
     # --- CONFIGURAR AS TECLAS GLOBAIS NO KeyManager ---
